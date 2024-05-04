f050b2a3d13bf9c6331ef2fc7358021584767395
DEV-01\developer <arfcwb@gmail.com>
2024-05-04 10:48:28 -0300

    habilitando viewBinding

--- app/src/main/java/com/example/androidaula/MainActivity.kt
+++ app/src/main/java/com/example/androidaula/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidaula.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    //private val binding by lazy {}
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
