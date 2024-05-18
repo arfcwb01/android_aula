5868b1328a10845e128a1da42467615ce2583649
guilhermegaijutis <gaijuter@gmail.com>
2024-05-18 12:14:57 -0300

    Numeros clicando

--- app/src/main/java/com/example/androidaula/MainActivity.kt
+++ app/src/main/java/com/example/androidaula/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.androidaula
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.androidaula.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
-       // ActivityMainBinding.inflate(layoutInflater)
+        // ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +22,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvDisplay: TextView = findViewById<TextView>(R.id.tv_display)
+        tvDisplay.text = "Guilherme"
+
+        val btn7: Button = findViewById(R.id.btn_7)
+        val btn8 : Button = findViewById(R.id.btn_8)
+        val btn9: Button = findViewById(R.id.btn_9)
+        val btnMais: Button = findViewById(R.id.btn_mais)
+        val btnLimpar: Button = findViewById(R.id.btn_limpar)
+        val btn0: Button = findViewById(R.id.btn_0)
+        val btnIgual: Button = findViewById(R.id.btn_igual)
+        val btnDividir: Button = findViewById(R.id.btn_dividir)
+
+        btn7.setOnClickListener { tvDisplay.text="7" }
+        btn8.setOnClickListener { tvDisplay.text="8" }
+        btn9.setOnClickListener { tvDisplay.text="9" }
+        btnMais.setOnClickListener { tvDisplay.text="+" }
+        btnLimpar.setOnClickListener { tvDisplay.text=" "}
+        btn0.setOnClickListener { tvDisplay.text="0" }
+        btnIgual.setOnClickListener { tvDisplay.text="="}
+        btnDividir.setOnClickListener { tvDisplay.text="/"}
+
+
+
+
     }
 }
