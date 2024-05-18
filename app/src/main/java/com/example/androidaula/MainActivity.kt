2a790c89949bd98133edaec64a089adb0cde2c5a
danielsgomes <danielilustrador@gmail.com>
2024-05-18 11:59:26 -0300

    modificaloes main

--- app/src/main/java/com/example/androidaula/MainActivity.kt
+++ app/src/main/java/com/example/androidaula/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.androidaula
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidaula.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -21,5 +24,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvDisplay:TextView=findViewById(R.id.tv_display)
+        tvDisplay.text"olá mundo"
+        val btn_4: Button=findViewById(R.id.btm_4)
+        val btn_5: Button=findViewById(R.id.btm_5)
+        val btn_6: Button=findViewById(R.id.btm_6)
+        val btn_subtracao: Button=findViewById(R.id.menos)
+
+
     }
 }
